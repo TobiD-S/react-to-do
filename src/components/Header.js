eca540e3bffaea1a7bec21a2e3a9d7f53638ecf4
Tobi <delesafatobi@gmail.com>
2020-05-10 11:43:38 +0100

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Header(){
+    return(
+       <div>
+           <nav>
+               <ul>
+                    <li>
+                        Hello
+                    </li>
+                    <li>
+                        Head
+                    </li>
+               </ul>
+           </nav>
+       </div> 
+    )
+}
+
+export default Header
